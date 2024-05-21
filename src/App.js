010eb905f74f9e77e313b2d970b5dba5bf8426ac
sachin121kum <sk31817@gmail.com>
2024-05-21 12:23:55 +0530

    first commit pwa app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar,Nav } from 'react-bootstrap';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Users from './Users';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+   <Navbar bg="primary" data-bs-theme="dark">
+          <Navbar.Brand >Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/about">About</Link></Nav.Link>
+            <Nav.Link><Link to="/users">Users</Link></Nav.Link>
+          </Nav>
+      </Navbar>
+      <Routes>
+  <Route path='/' element={<Home />} />
+  <Route path='/about' element={<About />} />
+  <Route path='/users' element={<Users />} />
+</Routes>
+      </Router>
     </div>
   );
 }
